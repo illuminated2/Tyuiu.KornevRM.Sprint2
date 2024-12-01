4f26fb883001da959e3a321338822a936b4922b5
illuminated2 <illuminated_89@mail.ru>
2024-12-01 15:45:03 +0500

    Task2

--- Tyuiu.KornevRM.Sprint2.Task1.V16/Program.cs
+++ Tyuiu.KornevRM.Sprint2.Task1.V16/Program.cs
@@ -6,6 +6,21 @@ namespace Tyuiu.KornevRM.Sprint2.Task1.V16
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Корнев Р.М. | ИСПб-24-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                         *");
+            Console.WriteLine("* Тема: Логические операции                                                         *");
+            Console.WriteLine("* Задание #1                                                                        *");
+            Console.WriteLine("* Вариант #16                                                                       *");
+            Console.WriteLine("* Выполнила: Корнев Р.М. | ИСПб-24-1                                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,                   *");
+            Console.WriteLine("* можно чередовать, но использовать один раз в выражении) и логических операций     *");
+            Console.WriteLine("* (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться), а также   *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность(массив):   *");
+            Console.WriteLine("* (True, False, False, False, False, False), при a = 145, b = 716, c = 144, d = 137 *");
+            Console.WriteLine("*                                                                                   *");
             int a = 145;
             int b = 716;
             int c = 144;
