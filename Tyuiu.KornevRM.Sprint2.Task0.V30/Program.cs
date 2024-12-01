97014549c42ac5966ffb505f31f09bc0e25a2a98
illuminated2 <illuminated_89@mail.ru>
2024-12-01 15:41:27 +0500

    Task0

--- Tyuiu.KornevRM.Sprint2.Task0.V30/Program.cs
+++ Tyuiu.KornevRM.Sprint2.Task0.V30/Program.cs
@@ -6,7 +6,20 @@ namespace Tyuiu.KornevRM.Sprint2.Task0.V30
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-
+            Console.Title = "Спринт #2 | Выполнила: Корнев Р.М. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнила: Корнев Р.М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (False, True, False, True, False, True), при x = 95, y = 1735           *");
+            Console.WriteLine("*                                                                         *");
             int x = 95;
             int y = 1735;
             bool[] res = new bool[6];
