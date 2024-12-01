b94a3e960229dedef0ad907e62071045ed2e9b4c
illuminated2 <illuminated_89@mail.ru>
2024-12-01 15:51:52 +0500

    Task4

--- Tyuiu.KornevRM.Sprint2.Task4.V9/Program.cs
+++ Tyuiu.KornevRM.Sprint2.Task4.V9/Program.cs
@@ -7,10 +7,24 @@ namespace Tyuiu.KornevRM.Sprint2.Task4.V9
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-
-            Console.WriteLine("***********************************************************************");
-            Console.WriteLine("*ИСКХОДНЫЕ ДАННЫЕ:                                                    *");
-            Console.WriteLine("***********************************************************************");
+            Console.Title = "Спринт #2 | Выполнила: Корнев Р.М. | ИСПб-24-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                         *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                          *");
+            Console.WriteLine("* Задание #4                                                                        *");
+            Console.WriteLine("* Вариант #9                                                                        *");
+            Console.WriteLine("* Выполнила: Корнев Р.М. | ИСПб-24-1                                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с использованием         *");
+            Console.WriteLine("* тернарного оператора, где пользователь вводит значение переменных x,y с           *");
+            Console.WriteLine("* клавиатуры, если x - 20 * 2 < y / 4 , то                                          *");
+            Console.WriteLine("* z = ( 3 + 8 / x^2) ^ y иначе y - ( x + 1 / y + 2) ^ x                             *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+           
 
             Console.WriteLine("Введите значение переменной X");
             double x = Convert.ToDouble(Console.ReadLine());
@@ -19,9 +33,9 @@ namespace Tyuiu.KornevRM.Sprint2.Task4.V9
             double y = Convert.ToDouble(Console.ReadLine());
             double res = ds.Calculate(x,y);
 
-            Console.WriteLine("***********************************************************************");
-            Console.WriteLine("*РЕЗУЛЬТАТ:                                                           *");
-            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                         *");
+            Console.WriteLine("*************************************************************************************");
             Console.WriteLine("Значение функции = " + res);
             Console.ReadKey();
 
