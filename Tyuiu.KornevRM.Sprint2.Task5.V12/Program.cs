2a07616e0074e711024aae228117092803ee8301
illuminated2 <illuminated_89@mail.ru>
2024-12-01 15:55:36 +0500

    Task5

--- Tyuiu.KornevRM.Sprint2.Task5.V12/Program.cs
+++ Tyuiu.KornevRM.Sprint2.Task5.V12/Program.cs
@@ -7,9 +7,26 @@ namespace Tyuiu.KornevRM.Sprint2.Task5.V12
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.WriteLine("***********************************************************************");
-            Console.WriteLine("*ИСКХОДНЫЕ ДАННЫЕ:                                                    *");
-            Console.WriteLine("***********************************************************************");
+            Console.Title = "Спринт #2 | Выполнила: Корнев Р.М. | ИСПб-24-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                         *");
+            Console.WriteLine("* Тема: Оператор switch                                                             *");
+            Console.WriteLine("* Задание #5                                                                        *");
+            Console.WriteLine("* Вариант #12                                                                       *");
+            Console.WriteLine("* Выполнила: Корнев Р.М. | ИСПб-24-1                                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Задание:                                                                          *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет                  *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                                        *");
+            Console.WriteLine("* Условие:                                                                          *");
+            Console.WriteLine("* Дата некоторого дня характеризуется тремя натуральными числами:                   *");
+            Console.WriteLine("* m(порядковый номер месяца) и n(число).По заданным g, n и m определить             *");
+            Console.WriteLine("* дату предыдущего дня.Заданный год является високосным.                            *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+    
             Console.WriteLine("Введите год:");
             int g = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите месяц:");
@@ -17,9 +34,9 @@ namespace Tyuiu.KornevRM.Sprint2.Task5.V12
             Console.WriteLine("Введите день:");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("***********************************************************************");
-            Console.WriteLine("*РЕЗУЛЬТАТ:                                                           *");
-            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                         *");
+            Console.WriteLine("*************************************************************************************");
 
             string res = Convert.ToString(ds.FindDateOfPreviousDay(g, m, n));
             Console.WriteLine(res);
